892d722035faf240d7eb04bb534895541f51562f
Lucieno <1155064923@link.cuhk.edu.hk>
2020-07-24 06:16:20 +0800

    usable

--- py_eth_pairing/__init__.py
+++ py_eth_pairing/__init__.py
@@ -1,5 +1,3 @@
-from time import time
-
 import eth_pairing_py
 from py_ecc.bn128 import G1, G2, add, multiply, curve_order, pairing
 
@@ -49,37 +47,10 @@ def pairing2(g1_1, g2_1, g1_2, g2_2):
   # c = lst_to_int(lst_c)
   return c
 
-print("add")
-t0 = time()
-actual = curve_add(G1, G1)
-print(time() - t0)
-expected = add(G1, G1)
-assert actual == expected
 
-print("mul")
-sc = curve_order - 10
-g1_pk = multiply(G1, sc)
-t0 = time()
-actual = curve_mul(g1_pk, sc)
-print(time() - t0)
-expected = multiply(g1_pk, sc)
-assert actual == expected
-
-print("pairing: expect true")
-sc = 123
-g1_pk = multiply(G1, sc)
-g2_pk = multiply(G2, sc)
-t0 = time()
-actual = pairing2(curve_negate(g1_pk), G2, G1, g2_pk)
-print(time() - t0)
-expected = True
-assert actual == expected
-
-print("pairing: expect false")
-g1_pk = multiply(G1, 123)
-g2_pk = multiply(G2, 1234)
-t0 = time()
-actual = pairing2(curve_negate(g1_pk), G2, G1, g2_pk)
-print(time() - t0)
-expected = False
-assert actual == expected
+__all__ = [
+    "search_py",
+    "search",
+    "search_sequential",
+    "search_sequential_allow_threads",
+]
---
+++ test.py
@@ -0,0 +1,39 @@
+from time import time
+
+from py_eth_pairing import curve_add, curve_mul, pairing2, curve_negate
+from py_ecc.bn128 import G1, G2, add, multiply, curve_order, pairing
+
+print("add")
+t0 = time()
+actual = curve_add(G1, G1)
+print(time() - t0)
+expected = add(G1, G1)
+assert actual == expected
+
+print("mul")
+sc = curve_order - 10
+g1_pk = multiply(G1, sc)
+t0 = time()
+actual = curve_mul(g1_pk, sc)
+print(time() - t0)
+expected = multiply(g1_pk, sc)
+assert actual == expected
+
+print("pairing: expect true")
+sc = 123
+g1_pk = multiply(G1, sc)
+g2_pk = multiply(G2, sc)
+t0 = time()
+actual = pairing2(curve_negate(g1_pk), G2, G1, g2_pk)
+print(time() - t0)
+expected = True
+assert actual == expected
+
+print("pairing: expect false")
+g1_pk = multiply(G1, 123)
+g2_pk = multiply(G2, 1234)
+t0 = time()
+actual = pairing2(curve_negate(g1_pk), G2, G1, g2_pk)
+print(time() - t0)
+expected = False
+assert actual == expected
